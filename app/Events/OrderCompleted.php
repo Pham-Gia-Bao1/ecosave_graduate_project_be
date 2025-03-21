301be1506578a38548b681eb10fab7dda6e71956
ThuyNgan015 <145478844+ThuyNgan015@users.noreply.github.com>
2025-03-21 20:24:58 +0700

    Feature/export revenue report
    * dump.dumprdb file
    
    * feature/export revenue report
    
    * export revenue report

---
+++ app/Events/OrderCompleted.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\Order;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class OrderCompleted implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $order;
+    public $storeId;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct(Order $order)
+    {
+        $this->order = $order;
+        $this->storeId = $order->store_id;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new PrivateChannel('store.' . $this->storeId);
+    }
+
+    /**
+     * Get the data to broadcast.
+     *
+     * @return array
+     */
+    public function broadcastWith()
+    {
+        return [
+            'order_id' => $this->order->id,
+            'store_id' => $this->storeId,
+            'total_price' => $this->order->total_price,
+            'timestamp' => now()->toDateTimeString()
+        ];
+    }
+}
